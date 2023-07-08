3e0ea4f9c66f71e5b566c3f3ee5eac8543edb8c5
David Foster <ifajl>
2023-07-08 19:50:07 -0400

    finished connect page and ministries ---- starting about page

--- website/views.py
+++ website/views.py
@@ -33,3 +33,32 @@ def connect():
 @views.route('/events')
 def events():
     return render_template("events.html")
+
+
+@views.route('/youth')
+def youth():
+    return render_template("youth.html")
+
+@views.route('/arts')
+def arts():
+    return render_template("music-finearts.html")
+
+@views.route('/sundayschool')
+def sundayschool():
+    return render_template("sundayschool.html")
+
+@views.route('/missions')
+def missions():
+    return render_template("missions.html")
+
+@views.route('/education')
+def education():
+    return render_template("education.html")
+
+@views.route('/aberdeen')
+def aberdeen():
+    return render_template("aberdeen.html")
+
+@views.route('/evangelism')
+def evangelism():
+    return render_template("evangelism-outreach.html")
